87ea179f81c554518cca694910f78810688513db
IndhuLekha0212 <indulekha@lendingkart.com>
2020-03-24 13:05:38 +0530

    Task OPS2-4530

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   
   title = 'project';
+  public name ="Status";
+  public selects =['Agreement sent','Pending Approval','T and C approved','Disbursed','Assigned to Credit Analysis','Analysis in progress','Agreement Signed',
+                    'T and C under Finailization','Application Rejected','Agreement in Progress'];
 }
---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+//import { Department } from '../models/department.model';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent implements OnInit {
+
+  @Input() public Cname;
+  @Input() public options;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
